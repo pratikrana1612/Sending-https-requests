36f2127bc9f48bfe35e16c577e329da32c27f99b
Rana Pratik <ranapratik1612@gmail.com>
2022-10-22 12:31:34 +0530

    promise version

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,15 +3,28 @@ const postTemplate = document.getElementById('single-post');
 
 function sendHttpRequest(method, url) 
 {
-    const xhr = new XMLHttpRequest();
-
-    xhr.open(method, url);
-
-    xhr.responseType = 'json';
-    xhr.onload = function () {
+    const promise = new Promise((resolved,reject) => {
+        const xhr = new XMLHttpRequest();
+    
+        xhr.open(method, url);
+        xhr.send();
         // const listOfPosts = JSON.parse(xhr.response);
-        const listOfPosts = xhr.response;
-        // console.log(listOfPosts);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            xhr.resolved(xhr.response);
+            // console.log(listOfPosts);
+        }
+    })
+    return promise;
+}
+
+
+
+function fetchPosts() 
+{
+    sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts').then(responseData =>
+    {
+        const listOfPosts = responseData;
         for (const post of listOfPosts) {
             const postEl = document.importNode(postTemplate.content, true);
             postEl.querySelector('h2').textContent = post.title.toUpperCase();
@@ -19,11 +32,9 @@ function sendHttpRequest(method, url)
             listElement.append(postEl);
             // console.log(postEl);
         }
-    }
-}
+    });
 
-sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts');
-xhr.send();
+}
 
 
 
